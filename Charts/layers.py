fafd8b0499fd569479ac61234658c4080f2500ef
Dimitri <DimitriBolt@Arizona.edu>
2024-08-11 02:03:54 -0700

    Сделал установку всх параметров с верхнего уровня.

--- Charts/layers.py
+++ Charts/layers.py
@@ -1,5 +1,10 @@
 import os
+
+import matplotlib
+import numpy
 import pandas
+
+print(matplotlib.__version__, numpy.__version__, pandas.__version__)
 import matplotlib.pyplot as plt
 
 training_loss_1 = "H1_2DLoss"
@@ -7,41 +12,39 @@ training_loss_2 = "L2_2Dloss"
 
 epoch = 20 - 1
 
-n_layers =  1
+n_layers = 1
 layer1h1 = pandas.read_pickle(os.path.join(os.path.expanduser('~'), 'Documents', training_loss_1 + "_l" + str(n_layers) + "_e" + str(epoch + 1) + '.pkl'))
 layer1l2 = pandas.read_pickle(os.path.join(os.path.expanduser('~'), 'Documents', training_loss_2 + "_l" + str(n_layers) + "_e" + str(epoch + 1) + '.pkl'))
 
-n_layers =  2
+n_layers = 2
 layer2h1 = pandas.read_pickle(os.path.join(os.path.expanduser('~'), 'Documents', training_loss_1 + "_l" + str(n_layers) + "_e" + str(epoch + 1) + '.pkl'))
 layer2l2 = pandas.read_pickle(os.path.join(os.path.expanduser('~'), 'Documents', training_loss_2 + "_l" + str(n_layers) + "_e" + str(epoch + 1) + '.pkl'))
 
-
-n_layers =  3
+n_layers = 3
 layer3h1 = pandas.read_pickle(os.path.join(os.path.expanduser('~'), 'Documents', training_loss_1 + "_l" + str(n_layers) + "_e" + str(epoch + 1) + '.pkl'))
 layer3l2 = pandas.read_pickle(os.path.join(os.path.expanduser('~'), 'Documents', training_loss_2 + "_l" + str(n_layers) + "_e" + str(epoch + 1) + '.pkl'))
 
-
-n_layers =  4
+n_layers = 4
 layer4h1 = pandas.read_pickle(os.path.join(os.path.expanduser('~'), 'Documents', training_loss_1 + "_l" + str(n_layers) + "_e" + str(epoch + 1) + '.pkl'))
 layer4l2 = pandas.read_pickle(os.path.join(os.path.expanduser('~'), 'Documents', training_loss_2 + "_l" + str(n_layers) + "_e" + str(epoch + 1) + '.pkl'))
 
-n_layers =  5
+n_layers = 5
 layer5h1 = pandas.read_pickle(os.path.join(os.path.expanduser('~'), 'Documents', training_loss_1 + "_l" + str(n_layers) + "_e" + str(epoch + 1) + '.pkl'))
 layer5l2 = pandas.read_pickle(os.path.join(os.path.expanduser('~'), 'Documents', training_loss_2 + "_l" + str(n_layers) + "_e" + str(epoch + 1) + '.pkl'))
 
-n_layers =  6
+n_layers = 6
 layer6h1 = pandas.read_pickle(os.path.join(os.path.expanduser('~'), 'Documents', training_loss_1 + "_l" + str(n_layers) + "_e" + str(epoch + 1) + '.pkl'))
 layer6l2 = pandas.read_pickle(os.path.join(os.path.expanduser('~'), 'Documents', training_loss_2 + "_l" + str(n_layers) + "_e" + str(epoch + 1) + '.pkl'))
 
-n_layers =  7
+n_layers = 7
 layer7h1 = pandas.read_pickle(os.path.join(os.path.expanduser('~'), 'Documents', training_loss_1 + "_l" + str(n_layers) + "_e" + str(epoch + 1) + '.pkl'))
 layer7l2 = pandas.read_pickle(os.path.join(os.path.expanduser('~'), 'Documents', training_loss_2 + "_l" + str(n_layers) + "_e" + str(epoch + 1) + '.pkl'))
 
-n_layers =  8
+n_layers = 8
 layer8h1 = pandas.read_pickle(os.path.join(os.path.expanduser('~'), 'Documents', training_loss_1 + "_l" + str(n_layers) + "_e" + str(epoch + 1) + '.pkl'))
 layer8l2 = pandas.read_pickle(os.path.join(os.path.expanduser('~'), 'Documents', training_loss_2 + "_l" + str(n_layers) + "_e" + str(epoch + 1) + '.pkl'))
 
-n_layers =  9
+n_layers = 9
 layer9h1 = pandas.read_pickle(os.path.join(os.path.expanduser('~'), 'Documents', training_loss_1 + "_l" + str(n_layers) + "_e" + str(epoch + 1) + '.pkl'))
 layer9l2 = pandas.read_pickle(os.path.join(os.path.expanduser('~'), 'Documents', training_loss_2 + "_l" + str(n_layers) + "_e" + str(epoch + 1) + '.pkl'))
 
@@ -68,17 +71,16 @@ layer_l2 = [layer1l2["train_err"][19],
             ]
 
 fig, ax = plt.subplots(figsize=(8, 8))
-ax.plot([1,2,3,4,5,6,7,8,9],
+ax.plot([1, 2, 3, 4, 5, 6, 7, 8, 9],
         layer_h1,
         label=training_loss_1 + ' for train',
         color='green',
         linestyle='dashed')
 
-ax.plot([1,2,3,4,5,6,7,8,9],
+ax.plot([1, 2, 3, 4, 5, 6, 7, 8, 9],
         layer_l2,
         label=training_loss_2 + ' for train',
         color='red')
-
 
 ax.legend(loc='upper right')
 ax.grid(which='major', alpha=0.5)
@@ -90,7 +92,6 @@ ax.set_title('Losses during training. \nTrain resolution = 16x16[const], test_re
 ax.set_xlabel('Layers')
 plt.grid()
 plt.show()
-
 
 pass
 
